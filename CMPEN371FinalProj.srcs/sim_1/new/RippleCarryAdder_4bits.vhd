da02c5f5f1a41e6db6acde9e8006e17d4e68b713
GabrielG <morgangenung242@gmail.com>
2024-12-06 13:55:23 -0500

    Update RippleCarryAdder_4bits.vhd

--- CMPEN371FinalProj.srcs/sim_1/new/RippleCarryAdder_4bits.vhd
+++ CMPEN371FinalProj.srcs/sim_1/new/RippleCarryAdder_4bits.vhd
@@ -1,7 +1,7 @@
 -- Dominic Cronaur, Gabriel Genung
 -- dlc5994@psu.edu, mxg1342@psu.edu
 -- RippleCarryAdder_4bit.vhd
--- Version 3.0 , 02/14/2023
+-- Version 3.0 , 12/6/2024
 
 -- This file implements a design of 4-bits Ripple Carry Adder
 library IEEE;
