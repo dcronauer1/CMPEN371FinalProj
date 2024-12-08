cb3c50d27f0f5267a1a4957a6cd1330ddf42eb05
GabrielG <morgangenung242@gmail.com>
2024-12-08 16:24:59 -0500

    Update FinalPROJ.vhd

--- CMPEN371FinalProj.srcs/sim_1/new/FinalPROJ.vhd
+++ CMPEN371FinalProj.srcs/sim_1/new/FinalPROJ.vhd
@@ -122,8 +122,11 @@ Reg2: PIPO_4bit port map(A=>Bin, LorS=>'0', Dir=>'0', Enclk=>MainClk, B=>B_Reg);
 Reg3: PIPO_4bit port map(A=>OP_Code, LorS=>'0', Dir=>'0', Enclk=>MainClk, B=>OP_Reg);
     --Need to add extra 1 bit register for extra data line
 ADemux: One_to_Sixteen4Bit_DEMUX port map(DataIn=>A_Reg, SEL=>OP_Reg, y0=>A_ADD, y1=>A_AND, y2=>A_OR, y3=>A_XOR, y4=>open, y5=>open, y6=>open, y7=>open, y7=>open, y8=>open, y9=>open, y10=>open, y11=>open, y12=>open, y13=>open, y14=>open, y15=>open);
-ADemux: One_to_Sixteen4Bit_DEMUX port map(DataIn=>B_Reg, SEL=>OP_Reg, y0=>B_ADD, y1=>B_AND, y2=>B_OR, y3=>B_XOR, y4=>open, y5=>open, y6=>open, y7=>open, y7=>open, y8=>open, y9=>open, y10=>open, y11=>open, y12=>open, y13=>open, y14=>open, y15=>open);
-
+BDemux: One_to_Sixteen4Bit_DEMUX port map(DataIn=>B_Reg, SEL=>OP_Reg, y0=>B_ADD, y1=>B_AND, y2=>B_OR, y3=>B_XOR, y4=>open, y5=>open, y6=>open, y7=>open, y7=>open, y8=>open, y9=>open, y10=>open, y11=>open, y12=>open, y13=>open, y14=>open, y15=>open);
+ADD1: RippleCarryAdder_4bit port map(EX_Cin=>Extra_Reg, EX_A=>A_ADD, EX_B=>B_ADD, EX_SUM=>Data_Out, EX_Cout=>Carry_Out);
+AND1: AND_Operation4bit port map(A=>A_AND, B=>B_AND, F=>Data_Out);
+OR1: OR_Operation4bit port map(A=>A_OR, B=>B_OR, F=>Data_Out);
+XOR1: XOR_Operation4bit port map(A=>A_XOR, B=>B_XOR, F=>Data_Out);
 
 
 end Behavioral;
